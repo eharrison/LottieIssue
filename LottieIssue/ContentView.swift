4311fe01a06f17f04ab34cdd8cbeecd34449c951
Stewart Lynch <slynch@createchsol.com>
2022-12-27 08:45:46 -0800

    Initial Commit

---
+++ LottieIssue/ContentView.swift
@@ -0,0 +1,42 @@
+//
+// Created for LottieIssue
+// by  Stewart Lynch on 2022-12-26
+// Using Swift 5.0
+// Running on macOS 13.1
+// 
+// Folllow me on Mastodon: @StewartLynch@iosdev.space
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var play = false
+    @State private var value = 0
+    var body: some View {
+        ZStack {
+            VStack {
+                Button("play") {
+                    value += 1
+                    play.toggle()
+                }
+                // Uncomment and this breaks
+//                Text("The Value is \(value)")
+                Rectangle()
+                    .frame(width: 400, height: 400)
+                    .foregroundColor(.clear)
+                    .overlay {
+                        LottieView(animationName: "confetti",
+                                    play: $play)
+                    }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LottieIssue/LottieView.swift
@@ -0,0 +1,57 @@
+//
+// Created for LottieIssue
+// by  Stewart Lynch on 2022-12-26
+// Using Swift 5.0
+// Running on macOS 13.1
+// 
+// Folllow me on Mastodon: @StewartLynch@iosdev.space
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    let animationName:String
+    
+    @Binding var play: Bool
+    var animationView:LottieAnimationView
+    
+    init(animationName:String,
+         play: Binding<Bool> = .constant(true)
+    ) {
+        self.animationName = animationName
+        self.animationView = LottieAnimationView(name: animationName)
+        self._play = play
+    }
+    class Coordinator: NSObject {
+        @Binding var play: Bool
+        let parent: LottieView
+        init(parent: LottieView, play: Binding<Bool>) {
+            self.parent = parent
+            _play = play
+        }
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(parent: self, play: $play)
+    }
+    
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView(frame: .zero)
+        view.addSubview(animationView)
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        animationView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        if play {
+            animationView.play { _ in
+                play.toggle()
+            }
+        }
+    }
+}
